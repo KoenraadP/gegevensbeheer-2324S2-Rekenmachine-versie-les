48483ee569ae4fe98595f53905acbca5c41c819f
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-05-02 11:47:02 +0200

    Add single function for buttons

--- scripts/rekenmachine.js
+++ scripts/rekenmachine.js
@@ -15,35 +15,21 @@ let subtractButton = document.getElementById("subtract-button");
 let multiplyButton = document.getElementById("multiply-button");
 let divideButton = document.getElementById("divide-button");
 
+// buttons samen in een array steken
+let buttons = [addButton, subtractButton, multiplyButton, divideButton];
+
 // variabelen maken voor opslag cijfers
 let number1 = 0;
 let number2 = 0;
 let result = 0;
 
-// click event toevoegen aan optel knop addButton
-addButton.addEventListener("click", function(){
-    ReadNumbers();
-    result = Calculate(number1, number2, "+");
-    resultOutput.value = parseFloat(result.toFixed(2));
-})
-
-subtractButton.addEventListener("click", function(){
-    ReadNumbers();
-    result = Calculate(number1, number2, "-");
-    resultOutput.value = parseFloat(result.toFixed(2));
-})
-
-multiplyButton.addEventListener("click", function(){
-    ReadNumbers();
-    result = Calculate(number1, number2, "x");
-    resultOutput.value = parseFloat(result.toFixed(2));
-})
-
-divideButton.addEventListener("click", function(){
-    ReadNumbers();
-    result = Calculate(number1, number2, ":");
-    resultOutput.value = parseFloat(result.toFixed(2));
-})
+// loop (foreach) gebruiken om click function
+// aan alle vier de knoppen toe te voegen
+buttons.forEach(button => {
+    button.addEventListener("click", function(e){
+        ClickButton(e.target.innerText);
+    })
+});
 
 // function die de cijfers inleest
 function ReadNumbers(){
@@ -67,5 +53,21 @@ function Calculate(number1, number2, operator){
     }
     if (operator == ":") {
         return number1 / number2;
+    }
+}
+
+// universele click function voor de buttons
+function ClickButton(buttonText) {
+    ReadNumbers();
+    // validatie op cijfers
+    // isNaN --> controleert of iets wel degelijk een cijfer is
+    // !isNaN() --> met uitroepteken: gaat door als het effectief
+    // WEL een cijfer is
+    if (!isNaN(number1) && !isNaN(number2)) {
+        result = Calculate(number1, number2, buttonText);
+        resultOutput.value = parseFloat(result.toFixed(2));
+    }
+    else {
+        alert("Geen geldige cijfers ingevuld!");
     }
 }
