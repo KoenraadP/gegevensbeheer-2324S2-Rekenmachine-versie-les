cc7480cbe7d3c02940ad5636877c7e2c16e1c513
Koenraad Pecceu <koenraad.pecceu@creo.be>
2024-05-02 11:11:54 +0200

    Add Calculate function

--- scripts/rekenmachine.js
+++ scripts/rekenmachine.js
@@ -22,48 +22,50 @@ let result = 0;
 
 // click event toevoegen aan optel knop addButton
 addButton.addEventListener("click", function(){
-    // waarde van input vakjes invullen
-    // bij variabelen number1 en number2
-    // parseFloat om te zorgen dat ze ingelezen worden als
-    // cijfers en niet als tekst
-    number1 = parseFloat(number1Input.value);
-    number2 = parseFloat(number2Input.value);
-    // test: console.log(number1);
-
-    // berekening uitvoeren
-    result = number1 + number2;
-
-    // resultaat tonen in vakje onderaan
-    // .toFixed om af te ronden met een specifiek
-    // aantal cijfers na de komma
-    // parseFloat om te zorgen dat er niet altijd cijfers staan
-    // na de komma
+    ReadNumbers();
+    result = Calculate(number1, number2, "+");
     resultOutput.value = parseFloat(result.toFixed(2));
 })
 
 subtractButton.addEventListener("click", function(){
-    number1 = parseFloat(number1Input.value);
-    number2 = parseFloat(number2Input.value);
-
-    result = number1 - number2;
-
+    ReadNumbers();
+    result = Calculate(number1, number2, "-");
     resultOutput.value = parseFloat(result.toFixed(2));
 })
 
 multiplyButton.addEventListener("click", function(){
-    number1 = parseFloat(number1Input.value);
-    number2 = parseFloat(number2Input.value);
-
-    result = number1 * number2;
-
+    ReadNumbers();
+    result = Calculate(number1, number2, "x");
     resultOutput.value = parseFloat(result.toFixed(2));
 })
 
 divideButton.addEventListener("click", function(){
-    number1 = parseFloat(number1Input.value);
-    number2 = parseFloat(number2Input.value);
-
-    result = number1 / number2;
-
+    ReadNumbers();
+    result = Calculate(number1, number2, ":");
     resultOutput.value = parseFloat(result.toFixed(2));
 })
+
+// function die de cijfers inleest
+function ReadNumbers(){
+    number1 = parseFloat(number1Input.value);
+    number2 = parseFloat(number2Input.value);
+}
+
+// function die een berekening uitvoert op basis
+// van de ingevoerde parameters bij de function
+// aanroepen is dan bvb: 
+// Calculate(number1, number2, "+")
+function Calculate(number1, number2, operator){
+    if (operator == "+") {
+        return number1 + number2;
+    }
+    if (operator == "-") {
+        return number1 - number2;
+    }
+    if (operator == "x") {
+        return number1 * number2;
+    }
+    if (operator == ":") {
+        return number1 / number2;
+    }
+}
